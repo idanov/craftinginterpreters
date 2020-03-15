6deb991fb33a955420c0ecbb894735a34976f865
Ivan Danov <idanov@users.noreply.github.com>
2020-03-15 01:01:08 +0000

    Parse identifiers

--- src/scanner.rs
+++ src/scanner.rs
@@ -57,7 +57,7 @@ impl<'a> Scanner<'a> {
 
     fn munch(&mut self, expected: char) -> bool {
         match self.chars.peek() {
-            Some(x) if *x == expected => {
+            Some(&x) if x == expected => {
                 self.advance();
                 true
             }
@@ -81,39 +81,69 @@ impl<'a> Scanner<'a> {
     fn scan_token(&mut self) {
         let c = self.advance();
         match c {
-            Some('(') => self.add_token(TokenType::LeftParen, c.unwrap().to_string()),
-            Some(')') => self.add_token(TokenType::RightParen, c.unwrap().to_string()),
-            Some('{') => self.add_token(TokenType::LeftBrace, c.unwrap().to_string()),
-            Some('}') => self.add_token(TokenType::RightBrace, c.unwrap().to_string()),
-            Some(',') => self.add_token(TokenType::Comma, c.unwrap().to_string()),
-            Some('.') => self.add_token(TokenType::Dot, c.unwrap().to_string()),
-            Some('-') => self.add_token(TokenType::Minus, c.unwrap().to_string()),
-            Some('+') => self.add_token(TokenType::Plus, c.unwrap().to_string()),
-            Some(';') => self.add_token(TokenType::Semicolon, c.unwrap().to_string()),
-            Some('*') => self.add_token(TokenType::Star, c.unwrap().to_string()),
+            Some(x @ '(') => self.add_token(TokenType::LeftParen, x.to_string()),
+            Some(x @ ')') => self.add_token(TokenType::RightParen, x.to_string()),
+            Some(x @ '{') => self.add_token(TokenType::LeftBrace, x.to_string()),
+            Some(x @ '}') => self.add_token(TokenType::RightBrace, x.to_string()),
+            Some(x @ ',') => self.add_token(TokenType::Comma, x.to_string()),
+            Some(x @ '.') => self.add_token(TokenType::Dot, x.to_string()),
+            Some(x @ '-') => self.add_token(TokenType::Minus, x.to_string()),
+            Some(x @ '+') => self.add_token(TokenType::Plus, x.to_string()),
+            Some(x @ ';') => self.add_token(TokenType::Semicolon, x.to_string()),
+            Some(x @ '*') => self.add_token(TokenType::Star, x.to_string()),
 
-            Some('!') if self.munch('=') => self.add_token(TokenType::BangEqual, "!=".to_string()),
-            Some('!') => self.add_token(TokenType::Bang, c.unwrap().to_string()),
-            Some('=') if self.munch('=') => self.add_token(TokenType::EqualEqual, "==".to_string()),
-            Some('=') => self.add_token(TokenType::Equal, c.unwrap().to_string()),
-            Some('<') if self.munch('=') => self.add_token(TokenType::LessEqual, "<=".to_string()),
-            Some('<') => self.add_token(TokenType::Less, c.unwrap().to_string()),
-            Some('>') if self.munch('=') => self.add_token(TokenType::GreaterEqual, ">=".to_string()),
-            Some('>') => self.add_token(TokenType::Greater, c.unwrap().to_string()),
+            Some(x @ '!') if self.munch('=') => self.add_token(TokenType::BangEqual, "!=".to_string()),
+            Some(x @ '!') => self.add_token(TokenType::Bang, x.to_string()),
+            Some(x @ '=') if self.munch('=') => self.add_token(TokenType::EqualEqual, "==".to_string()),
+            Some(x @ '=') => self.add_token(TokenType::Equal, x.to_string()),
+            Some(x @ '<') if self.munch('=') => self.add_token(TokenType::LessEqual, "<=".to_string()),
+            Some(x @ '<') => self.add_token(TokenType::Less, x.to_string()),
+            Some(x @ '>') if self.munch('=') => self.add_token(TokenType::GreaterEqual, ">=".to_string()),
+            Some(x @ '>') => self.add_token(TokenType::Greater, x.to_string()),
 
             Some('/') if self.munch('/') => {
-                    let _: String = self.chars.by_ref().take_while(|x| *x != '\n').collect();
+                    let _: String = self.chars.by_ref().take_while(|&x| x != '\n').collect();
                     self.line += 1;
                     self.current = 0;
                 },
-            Some('/') => self.add_token(TokenType::Slash, c.unwrap().to_string()),
+            Some(x @ '/') => self.add_token(TokenType::Slash, x.to_string()),
             Some(' ') | Some('\t') | Some('\r') => (),
             Some('\n') => {
                 self.line += 1;
                 self.current = 0;
             }
 
-            _ => (),
+            Some('"') => {
+                let mut lines = 0;
+                let res: String = self.chars.by_ref().take_while(|&x| {
+                    if x == '\n' {
+                        lines += 1;
+                    };
+                    x != '"'
+                }).collect();
+                self.add_token(TokenType::String, res);
+                self.line += lines;
+                // TODO: if self.chars.peek() is None (file ended), raise error for unterminated string
+            },
+
+            Some(x) if x.is_digit(10) => {
+                // TODO: parse numbers
+            },
+            Some(x) if x.is_alphabetic() => {
+                let mut ident:String = String::from(x.to_string());
+                ident.push(x);
+                while self.chars.peek().filter(|y| y.is_alphanumeric()).is_some() {
+                    ident.push(self.chars.next().unwrap());
+                };
+
+                // TODO: Check for keywords or just make it an identifier
+                self.add_token(TokenType::Identifier, ident);
+            },
+
+            _ => {
+                // raise error for unexpected character
+                ()
+            },
         }
     }
 
