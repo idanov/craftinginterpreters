0db2e0d1c8626218d1f955603c5fcbe11cd4d1d1
Ivan Danov <idanov@users.noreply.github.com>
2024-03-03 21:05:11 +0000

    Add runtime errors in the interpreter (ch. 7.3)

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -6,7 +6,7 @@ pub struct Interpreter {
 
 
 impl Interpreter {
-    pub fn evaluate(&mut self, expr: &Expr) -> Lit {
+    pub fn evaluate(&mut self, expr: &Expr) -> Result<Lit, String> {
         match expr {
             Expr::Binary(left, op, right) => self.eval_binary(left, op, right),
             Expr::Grouping(expr) => self.eval_grouping(expr),
@@ -19,39 +19,48 @@ impl Interpreter {
         self.evaluate(expr);
     }
 
-    fn eval_binary(&mut self, left: &Expr, op: &Token, right: &Expr) -> Lit {
-        let lval = self.evaluate(left);
-        let rval = self.evaluate(right);
+    fn eval_binary(&mut self, left: &Expr, op: &Token, right: &Expr) -> Result<Lit, String> {
+        let lval = self.evaluate(left)?;
+        let rval = self.evaluate(right)?;
         match (&lval, op.token, &rval) {
-            (Lit::Double(lhs), TT::Minus, Lit::Double(rhs)) => Lit::Double(lhs - rhs),
-            (Lit::Double(lhs), TT::Slash, Lit::Double(rhs)) => Lit::Double(lhs / rhs),
-            (Lit::Double(lhs), TT::Star, Lit::Double(rhs)) => Lit::Double(lhs * rhs),
-            (Lit::Double(lhs), TT::Plus, Lit::Double(rhs)) => Lit::Double(lhs + rhs),
-            (Lit::String(lhs), TT::Plus, Lit::String(rhs)) => Lit::String(lhs.to_string() + rhs),
-            (Lit::Double(lhs), TT::Greater, Lit::Double(rhs)) => Lit::Boolean(lhs > rhs),
-            (Lit::Double(lhs), TT::GreaterEqual, Lit::Double(rhs)) => Lit::Boolean(lhs >= rhs),
-            (Lit::Double(lhs), TT::Less, Lit::Double(rhs)) => Lit::Boolean(lhs < rhs),
-            (Lit::Double(lhs), TT::LessEqual, Lit::Double(rhs)) => Lit::Boolean(lhs <= rhs),
-            (_, TT::EqualEqual, _) => Lit::Boolean(Interpreter::is_equal(&lval, &rval)),
-            (_, TT::BangEqual, _) => Lit::Boolean(!Interpreter::is_equal(&lval, &rval)),
-            _ => Lit::None,
+            (Lit::Double(lhs), TT::Minus, Lit::Double(rhs)) => Ok(Lit::Double(lhs - rhs)),
+            (Lit::Double(lhs), TT::Slash, Lit::Double(rhs)) => Ok(Lit::Double(lhs / rhs)),
+            (Lit::Double(lhs), TT::Star, Lit::Double(rhs)) => Ok(Lit::Double(lhs * rhs)),
+            (_, TT::Minus, _) => Err("Operands must be numbers.".to_string()),
+            (_, TT::Slash, _) => Err("Operands must be numbers.".to_string()),
+            (_, TT::Star, _) => Err("Operands must be numbers.".to_string()),
+            (Lit::Double(lhs), TT::Plus, Lit::Double(rhs)) => Ok(Lit::Double(lhs + rhs)),
+            (Lit::String(lhs), TT::Plus, Lit::String(rhs)) => Ok(Lit::String(lhs.to_string() + rhs)),
+            (_, TT::Plus, _) => Err("Operands must be two numbers or two strings.".to_string()),
+            (Lit::Double(lhs), TT::Greater, Lit::Double(rhs)) => Ok(Lit::Boolean(lhs > rhs)),
+            (Lit::Double(lhs), TT::GreaterEqual, Lit::Double(rhs)) => Ok(Lit::Boolean(lhs >= rhs)),
+            (Lit::Double(lhs), TT::Less, Lit::Double(rhs)) => Ok(Lit::Boolean(lhs < rhs)),
+            (Lit::Double(lhs), TT::LessEqual, Lit::Double(rhs)) => Ok(Lit::Boolean(lhs <= rhs)),
+            (_, TT::Greater, _) => Err("Operands must be numbers.".to_string()),
+            (_, TT::GreaterEqual, _) => Err("Operands must be numbers.".to_string()),
+            (_, TT::Less, _) => Err("Operands must be numbers.".to_string()),
+            (_, TT::LessEqual, _) => Err("Operands must be numbers.".to_string()),
+            (_, TT::EqualEqual, _) => Ok(Lit::Boolean(Interpreter::is_equal(&lval, &rval))),
+            (_, TT::BangEqual, _) => Ok(Lit::Boolean(!Interpreter::is_equal(&lval, &rval))),
+            _ => Ok(Lit::None),
         }
     }
 
-    fn eval_grouping(&mut self, expr: &Expr) -> Lit {
+    fn eval_grouping(&mut self, expr: &Expr) -> Result<Lit, String> {
         return self.evaluate(expr);
     }
 
-    fn eval_literal(&mut self, lit: &Lit) -> Lit {
-        return lit.clone();
+    fn eval_literal(&mut self, lit: &Lit) -> Result<Lit, String> {
+        return Ok(lit.clone());
     }
 
-    fn eval_unary(&mut self, op: &Token, expr: &Expr) -> Lit {
-        let lit = self.evaluate(expr);
+    fn eval_unary(&mut self, op: &Token, expr: &Expr) -> Result<Lit, String> {
+        let lit = self.evaluate(expr)?;
         match (op.token, &lit) {
-            (TT::Minus, Lit::Double(n)) => Lit::Double(-n),
-            (TT::Bang, _) => Lit::Boolean(!Interpreter::is_truthy(&lit)),
-            _ => Lit::None,
+            (TT::Minus, Lit::Double(n)) => Ok(Lit::Double(-n)),
+            (TT::Minus, _) => Err("Operand must be a number.".to_string()),
+            (TT::Bang, _) => Ok(Lit::Boolean(!Interpreter::is_truthy(&lit))),
+            _ => Ok(Lit::None),
         }
     }
 
