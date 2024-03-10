d21933f4e910a2b686249a8a78ba7c543b2e1516
Ivan Danov <idanov@users.noreply.github.com>
2024-03-10 15:38:58 +0000

    Add enclosing environment for scopes (ch. 8.5)

--- src/environment.rs
+++ src/environment.rs
@@ -2,24 +2,36 @@ use crate::scanner::{Literal, Token};
 use std::collections::HashMap;
 
 pub struct Environment {
+    enclosing: Option<Box<Environment>>,
     values: HashMap<String, Literal>,
 }
 
 impl Environment {
     pub fn new() -> Self {
         Environment {
+            enclosing: None,
             values: HashMap::new(),
         }
     }
+
+    pub fn nested(enclosing: Environment) -> Self {
+        Environment {
+            enclosing: Some(Box::new(enclosing)),
+            values: HashMap::new(),
+        }
+    }
+
     pub fn define(&mut self, key: String, value: Literal) {
         self.values.insert(key, value);
     }
 
     pub fn get(&self, key: &Token) -> Result<Literal, String> {
-        self.values
+        return self
+            .values
             .get(&key.lexeme)
             .cloned()
-            .ok_or(format!("Undefined variable '{}'.", key.lexeme))
+            .or_else(|| self.enclosing.as_ref().and_then(|x| x.get(&key).ok()))
+            .ok_or(format!("Undefined variable '{}'.", key.lexeme));
     }
 
     pub fn assign(&mut self, name: &Token, val: Literal) -> Result<Literal, String> {
@@ -27,6 +39,11 @@ impl Environment {
             self.values.insert(name.lexeme.clone(), val.clone());
             return Ok::<Literal, String>(val);
         }
+
+        if let Some(x) = &mut self.enclosing {
+            return x.assign(name, val);
+        }
+
         return Err(format!("Undefined variable '{}'.", name.lexeme));
     }
 }
